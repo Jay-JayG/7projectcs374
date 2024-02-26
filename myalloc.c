68ad94af490be033b904c5796428e1a279398eac
Jay-JayG <jjgochez@gmail.com>
2024-02-25 22:51:00 -0800

    myfree and not segmenting if not needed implemented

--- myalloc.c
+++ myalloc.c
@@ -85,12 +85,4 @@ void print_data(void)
 }
 
 void main(void) {
-    void *p;
-
-    myalloc(10);     print_data();
-    p = myalloc(20); print_data();
-    myalloc(30);     print_data();
-    myfree(p);       print_data();
-    myalloc(40);     print_data();
-    myalloc(10);     print_data();
 }
